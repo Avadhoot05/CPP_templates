d80fcf0511df82af858ab6c8bedbae863ecae9c9
Avadhoot Khedekar <48721257+Avadhoot05@users.noreply.github.com>
2023-12-10 21:55:06 +0530

    modular exponent template

---
+++ Modular_exponent.cpp
@@ -0,0 +1,15 @@
+int power(long long x, unsigned int y, int mod) 
+{ 
+    int res = 1;     
+    x = x % mod; 
+    if (x == 0) return 0; 
+    while (y > 0) 
+    { 
+        if (y & 1) 
+            res = (res*x) % mod; 
+ 
+        y = y>>1; 
+        x = (x*x) % mod; 
+    } 
+    return res; 
+} 
